2e0be205fb900cbcfbc64bb1d5788d150ea65f6b
PAVEL TKACHEV <phoenixweiss@ya.ru>
2024-09-15 19:29:15 +0300

    total logic rework

--- bumpster.sh
+++ bumpster.sh
@@ -1,187 +1,231 @@
 #!/bin/bash
 
-# This command enables strict mode,
-# which treats unset variables as errors.
-# TODO: make the code comply with the requirement
-# set -u
+# Enable strict mode
+set -u
 
 ### Begin define variables ###
 
-# Great idea to safely define home by Ben Hoskings, author of "babushka" https://github.com/benhoskings/babushka
+# Home and project paths
 home=$(sh -c "echo ~$(whoami)")
-# Checks OS type
-ostype=$(uname -s)
-# Source
-from="https://github.com/phoenixweiss/bumpster"
-# Destination
-to="$home/.bumpster"
-# Full path to the "config" file in the ".git" folder
-config_path=".git/config"
+project_dir=$(pwd)
+
+# Global and local config file paths
+global_config_file="$home/.bumpsterrc"
+local_config_file="$project_dir/.bumpsterrc"
+
+# Default values
+default_master_branch="master"
+default_develop_branch="develop"
+default_logging="false"
+
+# Config variables
+master_branch=""
+develop_branch=""
+logging_enabled=""
+
+# Define version file location
+version_file="$home/.bumpster/VERSION"
 
 ### End define variables ###
 
-# This function is defined to print an error message
-# and exit with a status code of 1.
+# Function to display the version of Bumpster
+display_version() {
+  if [ -f "$version_file" ]; then
+    cat "$version_file"
+  else
+    echo "Version information not available."
+  fi
+}
+
+# Function to print an error message and exit with a status code of 1.
 abort() {
   printf "%s\n" "$@" >&2
   exit 1
 }
 
-# Function to remove newline character from a string
-remove_newline() {
-  local string=$1
-
-  # Check if the string contains a newline character
-  if [[ $string == *$'\n' ]]; then
-      # Remove the newline character from the end of the string
-      string=${string%"$'\n'"}
+# Function to log actions if logging is enabled
+log() {
+  if [[ "$logging_enabled" == "true" ]]; then
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - $1" >> bumpster.log
   fi
-
-  # Print the modified string
-  printf "$string"
 }
 
-# The conditional statement checks if the BASH_VERSION variable is empty,
-# indicating that Bash is not available, and calls the abort() function.
-# shellcheck disable=SC2292
-if [ -z "${BASH_VERSION:-}" ]
-then
-  abort "Bash is required to run this script."
+# Function to create a config file with the given path and values
+create_config() {
+  local config_file="$1"
+  local master_branch="$2"
+  local develop_branch="$3"
+  local logging="$4"
+
+  # Use a clean and correctly formatted here-document
+  cat > "$config_file" <<EOF
+# Bumpster configuration file
+# You can change the values here to configure the behavior of Bumpster
+
+# Git master branch (default: master)
+GIT_MASTER_BRANCH="$master_branch"
+
+# Git develop branch (default: develop)
+GIT_DEVELOP_BRANCH="$develop_branch"
+
+# Enable or disable logging (default: false)
+ENABLE_LOGGING="$logging"
+EOF
+  echo "Configuration file '$config_file' created."
+}
+
+# Function to run an interactive session for configuration
+interactive_setup() {
+  echo "Welcome to Bumpster setup!"
+  read -p "Enter the name for the master branch [default: $default_master_branch]: " master_branch_input
+  master_branch=${master_branch_input:-$default_master_branch}
+
+  read -p "Enter the name for the develop branch [default: $default_develop_branch]: " develop_branch_input
+  develop_branch=${develop_branch_input:-$default_develop_branch}
+
+  read -p "Enable logging? (y/n) [default: no]: " logging_input
+  logging_enabled="false"
+  if [[ "$logging_input" == "y" || "$logging_input" == "Y" ]]; then
+    logging_enabled="true"
+  fi
+
+  # Create the global config file based on user input
+  create_config "$global_config_file" "$master_branch" "$develop_branch" "$logging_enabled"
+}
+
+# Function to load configuration from a config file
+load_config() {
+  if [ -f "$1" ]; then
+    source "$1"
+    master_branch="${GIT_MASTER_BRANCH:-$default_master_branch}"
+    develop_branch="${GIT_DEVELOP_BRANCH:-$default_develop_branch}"
+    logging_enabled="${ENABLE_LOGGING:-$default_logging}"
+  fi
+}
+
+# Check if either local or global config exists, load the local config first
+if [ -f "$local_config_file" ]; then
+  echo "Using local configuration from '$local_config_file'."
+  load_config "$local_config_file"
+  elif [ -f "$global_config_file" ]; then
+  echo "Using global configuration from '$global_config_file'."
+  load_config "$global_config_file"
+else
+  echo "No configuration file found."
+  interactive_setup
 fi
 
-# The usage() function is defined to display a usage message
-# when the script is called with the -h or --help options.
+# Function to show usage and version information
 usage() {
   cat ./lib/BUMPSTER_LOGO.ASCII
   cat <<EOS
-Bumpster
+Bumpster $(display_version)
 Usage:  ./bumpster.sh [options]
         -h, --help      Display this message
         -M, --major     Bump major version
         -m, --minor     Bump minor version
         -p, --patch     Bump patch version
+        --version       Display the current version of Bumpster
 EOS
-  # The script exits with the status code passed as an argument
-  # to the exit command, defaulting to 0 if no argument is provided.
   exit "${1:-0}"
 }
 
-# Loop until all arguments are processed.
-while [[ $# -gt 0 ]]
-do
+# Check command-line options
+version_type=""
+while [[ $# -gt 0 ]]; do
   case "$1" in
-    # If the argument is -h or --help, call the usage function
-    -h | --help)    usage
-                    ;;
-    # If the argument is -M or --major
-    -M | --major )  version_type="major"
-                    ;;
-    # If the argument is -m or --minor
-    -m | --minor )  version_type="minor"
-                    ;;
-    # If the argument is -p or --patch
-    -p | --patch )  version_type="patch"
-                    ;;
-    # If the argument is anything else
-    *)
-      # Print an error message to stderr
-      printf "Unknown option: '$1'\n" >&2
-      # Call the usage function with exit code 1
-      usage 1
-      ;;
+    -h | --help)    usage ;;
+    --version)      echo "Bumpster version: $(display_version)"
+    exit 0 ;;
+    -M | --major )  version_type="major" ;;
+    -m | --minor )  version_type="minor" ;;
+    -p | --patch )  version_type="patch" ;;
+    *)              printf "Unknown option: '$1'\n" >&2
+    usage 1 ;;
   esac
+  shift
 done
 
-# Checking if the git flow repository is initialized
-if ! git rev-parse --git-dir >/dev/null 2>&1
-then
+# Ensure Bash is available
+if [ -z "${BASH_VERSION:-}" ]; then
+  abort "Bash is required to run this script."
+fi
+
+# Ensure necessary commands are available
+for cmd in git git-flow; do
+  if ! which "$cmd" >/dev/null 2>&1; then
+    abort "Error: $cmd is not installed. Please install it and try again."
+  fi
+done
+
+# Ensure the script is run in a git repository
+if ! git rev-parse --git-dir >/dev/null 2>&1; then
   abort "Git repository not found. Please initialize git first."
 fi
 
-# Checking for uncommitted changes
-if [[ -n $(git status --porcelain) ]]
-then
+# Ensure there are no uncommitted changes
+if [[ -n $(git status --porcelain) ]]; then
   abort "Working tree contains unstaged changes. Aborting."
 fi
 
-# Checking if git flow installed
-if ! which git-flow >/dev/null 2>&1 || ! git flow version >/dev/null 2>&1
-then
-  abort "Error: git flow is not installed. Please install it and try again."
-fi
+# Check if git flow is initialized and get branch names
+if grep -q "\[gitflow \"branch\"\]" ".git/config"; then
+  gf_master_branch_name=$(grep -oP 'master\s*=\s*\K.*' ".git/config" | sed 's/^[[:space:]]*//; s/[[:space:]]*$//')
+  gf_develop_branch_name=$(grep -oP 'develop\s*=\s*\K.*' ".git/config" | sed 's/^[[:space:]]*//; s/[[:space:]]*$//')
 
-# Check if the block [gitflow "branch"] is present in the "config" file
-if grep -q "\[gitflow \"branch\"\]" "$config_path"
-then
-  # Find string 'master = <value>' and assign it to gf_master_branch_name
-  gf_master_branch_name=$(grep -oP 'master\s*=\s*\K.*' "$config_path" | sed 's/^[[:space:]]*//; s/[[:space:]]*$//')
-
-  # Find string 'develop = <value>' and assign it to gf_develop_branch_name
-  gf_develop_branch_name=$(grep -oP 'develop\s*=\s*\K.*' "$config_path" | sed 's/^[[:space:]]*//; s/[[:space:]]*$//')
-
-  # Check that the gf_master_branch_name variable is not empty
-  if [[ -n $gf_master_branch_name ]]
-  then
-    echo "Git flow master branch name: $gf_master_branch_name"
-  else
-    abort "String 'master = <value>' did not found. Please run 'git flow init' first."
-  fi
-
-  # Check that the gf_develop_branch_name variable is not empty
-  if [[ -n $gf_develop_branch_name ]]
-  then
-    echo "Git flow develop branch name: $gf_develop_branch_name"
-  else
-    abort "String 'develop = <value>' did not found. Please run 'git flow init' first."
-  fi
+  # Use default names if not found
+  gf_master_branch_name=${gf_master_branch_name:-$master_branch}
+  gf_develop_branch_name=${gf_develop_branch_name:-$develop_branch}
 else
   abort "Git flow is not initialized. Please run 'git flow init' first."
 fi
 
-# Checking the existence of the VERSION file
-if [ -f "VERSION" ]
-then
-  # If flie exists get the current version from the VERSION file
+# Ensure VERSION file exists and read the current version
+if [ -f "VERSION" ]; then
   current_version=$(cat VERSION)
   echo "Current version is $current_version"
 else
-  # If the file does not exist, create a new one and write version 0.0.1 there
   current_version="0.0.1"
   printf $current_version > VERSION
   echo "The VERSION file is created and filled with the value $current_version"
 fi
 
-# If the user has not specified the version type, we ask the user directly
-if [ -z "$version_type" ]
-then
+# Prompt for version type if not provided
+if [ -z "$version_type" ]; then
   read -p "Which version do you want to bump (major/minor/patch)? [patch]: " version_type
-
-  # If the user has not entered anything, we use the patch version by default
-  if [ -z "$version_type" ]
-  then
-    version_type="patch"
-  fi
+  version_type=${version_type:-patch}
 fi
 
-# We change the version in accordance with the selected type
-new_version=$(awk -F. '{printf("%d.%d.%d", $1, $2, $3+1)}' <<< $current_version)
-case $version_type in
-  major) new_version=$(awk -F. '{printf("%d.%d.%d", $1+1, 0, 0)}' <<< $current_version);;
-  minor) new_version=$(awk -F. '{printf("%d.%d.%d", $1, $2+1, 0)}' <<< $current_version);;
-  patch) new_version=$(awk -F. '{printf("%d.%d.%d", $1, $2, $3+1)}' <<< $current_version);;
-esac
+# Validate version type
+if [[ "$version_type" != "major" && "$version_type" != "minor" && "$version_type" != "patch" ]]; then
+  abort "Invalid version type. Please choose between 'major', 'minor', or 'patch'."
+fi
 
-# Update the version value in the VERSION file and create a commit
+# Parse the current version and bump it
+IFS='.' read -r major minor patch <<< "$current_version"
+case $version_type in
+  major) ((major++)); minor=0; patch=0 ;;
+  minor) ((minor++)); patch=0 ;;
+  patch) ((patch++)) ;;
+esac
+new_version="$major.$minor.$patch"
+
+# Check if the new version is different from the current one
+if [[ "$current_version" == "$new_version" ]]; then
+  abort "New version is the same as the current version."
+fi
+
+# Update the VERSION file and create a commit
 printf $new_version > VERSION
 git add VERSION
 git commit -m "bump version to $new_version" -m "Automatic version bump to $new_version"
+log "Bumping version to $new_version"
 
-# Create a release branch from the develop branch in accordance with Git Flow
-git checkout $gf_develop_branch_name
-git flow release start $new_version
+# Create a release branch and finalize the release with git flow
+git checkout "$gf_develop_branch_name"
+git flow release start "$new_version"
+GIT_MERGE_AUTOEDIT=no git flow release finish -m "Release $new_version" -m "Automatic release $new_version" "$new_version"
 
-# We complete the release branch and create a tag for the new version
-GIT_MERGE_AUTOEDIT=no git flow release finish -m "Release $new_version" -m "Automatic release $new_version" $new_version
-
-# Push changes to the server
-git push origin $gf_develop_branch_name && git push origin $gf_master_branch_name --tags
+# Push changes to the repository
+git push origin "$gf_develop_branch_name" && git push origin "$gf_master_branch_name" --tags
+log "Release $new_version pushed to remote repository"
